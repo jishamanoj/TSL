e3b17421fad587cf71b7bba2978a41e45a1efc2e
jishamanoj <131763137+jishamanoj@users.noreply.github.com>
2023-12-20 12:06:18 +0530

    Update timeTracking.js

--- model/timeTracking.js
+++ model/timeTracking.js
@@ -4,6 +4,7 @@ const { DataTypes, Sequelize } = require('sequelize');
 //const bcrypt = require('bcrypt');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     dialect: process.env.DB_DIALECT,
+        host: process.env.DB_HOST,
     logging: false,
 
 });
