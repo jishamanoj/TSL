c008b87996e4a3d6a1968951ba83d21e6b6e0471
jishamanoj <131763137+jishamanoj@users.noreply.github.com>
2023-12-20 12:05:46 +0530

    Update question.js

--- model/question.js
+++ model/question.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { DataTypes, Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     dialect: process.env.DB_DIALECT,
+        host: process.env.DB_HOST,
     logging: false,
 
 });
